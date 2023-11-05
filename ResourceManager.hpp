5bd446d70b81c6ae7505a31adea55f73c84f5954
Maks7776 <134312065+Maks7776@users.noreply.github.com>
2023-11-05 12:14:55 +0100

    Add files via upload

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,33 @@
 #pragma once
 
-#include "Resource.hpp"
+#include "include/Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public:
+    ResourceManager()
+    {
+        res_ref = new Resource;
+    }
+
+    ~ResourceManager()
+    {
+        delete res_ref;
+    }
+
+    ResourceManager(const ResourceManager& ResMgr) : res_ref{ResMgr.res_ref} {}
+
+    ResourceManager& operator=(const ResourceManager& other_res)
+    {
+        res_ref = other_res.res_ref;
+        return *this;
+    }
+
+    double get()
+    {
+        return res_ref->get();
+    }
+
+    private:
+    Resource* res_ref;
 };
